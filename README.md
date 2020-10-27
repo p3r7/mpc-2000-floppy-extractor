467c0a79f5f601212c23a2096c09aeaf8ce2106a
p3r7 <11557146+p3r7@users.noreply.github.com>
2020-10-27 15:30:52 +0100

    Actual code

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# mpc-2000-floppy-extractor
-Extract files from an Akai MPC 2000 floppy 
+# MPC 2000 floppy extractor
+
+Extract files from an Akai MPC 2000 floppy image.
+
+
+## About
+
+The Akai MPC 2000 has a custom scheme for supporting long file names (LFN) on FAT12 filesystems.
+
+Its implementation is different from vFAT and uses the reserved space for each record entry in the root directory index.
+
+As a result, `mtools` and `mount` are unable to get retrieve the long file names associated with each file.
+
+
+## Implementation details
+
+This script uses the excellent [kaitai](https://kaitai.io/) binary parser to extract the fylesystem metadata (file names and position in binary) and then uses native python file operation to extract each file.
---
+++ main.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import kaitaistruct
+from kaitaistruct import KaitaiStruct, KaitaiStream, BytesIO
+
+from vfat import Vfat
+from pprint import pprint
+
+
+## ------------------------------------------------------------------------
+## CONF
+
+img_file = "/home/me/Documents/floppy_1.img"
+out_folder= "/tmp/out_mpc_floppy/"
+
+
+## ------------------------------------------------------------------------
+## PARSE FILES
+
+data = Vfat.from_file(img_file)
+
+# those might always the same for FAT12 but whatever...
+bytes_per_ls = data.boot_sector.bpb.bytes_per_ls
+ls_per_clus = data.boot_sector.bpb.ls_per_clus
+clus_size = bytes_per_ls * ls_per_clus
+
+data_start_clus = 33
+# cf https://www.eit.lth.se/fileadmin/eit/courses/eitn50/Literature/fat12_description.pdf
+
+start_clus_offset = None
+parsed_files = []
+
+for r in data.root_dir.records:
+    # NB: the records index is at 0x2600
+
+    if r.attribute == 0: # not an actual file
+        continue
+
+    sfn_no_ext = r.file_name[:-3].decode(u"ASCII")
+    ext = r.file_name[-3:].decode(u"ASCII")
+
+    # NB: MPC implementation od LFN uses reserved bytes of a record instead of separate record
+    lfn_part = r.reserved[:-2].decode(u"ASCII").rstrip()
+    lfn = sfn_no_ext + lfn_part + "." + ext
+
+    print("- " + lfn)
+    print("  start cluster:" + str(r.start_clus))
+    print("  size:" + str(r.file_size))
+
+    if start_clus_offset is None:
+        start_bytes = data_start_clus * clus_size
+        start_clus_offset = r.start_clus
+    else:
+        start_bytes = (data_start_clus - start_clus_offset + r.start_clus) * clus_size
+
+    print("  start pos in img: " + str(start_bytes))
+
+    parsed_files.append({
+        'name': lfn,
+        'start': start_bytes,
+        'size': size,
+    })
+
+
+## ------------------------------------------------------------------------
+## EXTRACT
+
+with open(img_file, 'rb') as f:
+    for props in parsed_files:
+        f.seek(props['start'], 0)
+        file_bytes = f.read(props['size'])
+        with open(out_folder + props['name'], "wb") as out_f:
+            out_f.write(file_bytes)
+            out_f.close()
+    f.close()
+
+print("Extraction complete!")
